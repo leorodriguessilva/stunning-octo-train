ad3f8c159cfa1ada8c7f2e004ede3bf12b00ef78
Leonardo Rodrigues da Silva <leonardo_faust@hotmail.com>
2026-01-14 00:43:20 +0100

    remove unnecessary transactional annotation from TodoItemRepository

--- src/main/kotlin/com/service/todolist/repository/TodoItemRepository.kt
+++ src/main/kotlin/com/service/todolist/repository/TodoItemRepository.kt
@@ -7,13 +7,11 @@ import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Modifying
 import org.springframework.data.jpa.repository.Query
 import org.springframework.data.repository.query.Param
-import org.springframework.transaction.annotation.Transactional
 import java.time.Instant
 
 interface TodoItemRepository : JpaRepository<TodoItem, Long> {
 	fun findByStatus(status: TodoStatus, sort: Sort = Sort.by("creationDatetime").ascending()): List<TodoItem>
 
-	@Transactional
 	@Modifying(flushAutomatically = true, clearAutomatically = true)
 	@Query(
 		"""
